ff3b459f148751f0195d1e2acc1ceb1c59c07381
zonieedhossain <zonieed.uap@gmail.com>
2019-09-11 15:26:52 +0600

    Basic App With Gorm

--- maiin.go
+++ maiin.go
@@ -17,7 +17,7 @@ func handleRequests() {
 	myRouter.HandleFunc("/", helloworld).Methods("GET")
 	myRouter.HandleFunc("/users", AllUsers).Methods("GET")
 	myRouter.HandleFunc("/users/{name}/{email}", NewUser).Methods("POST")
-	myRouter.HandleFunc("/users/{name}", DeleteUser).Methods("POST")
+	myRouter.HandleFunc("/users/{name}", DeleteUser).Methods("DELETE")
 	myRouter.HandleFunc("/users/{name}/{email}", UpdateUser).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":8888", myRouter))
--- user.go
+++ user.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/mux"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -14,12 +16,13 @@ var err error
 
 type User struct {
 	gorm.Model
-	user  string
-	email string
+	Name  string
+	Email string
 }
 
 func InitialMigration() {
-	db, err := gorm.Open("sqlite3", "test.db")
+
+	db, err := gorm.Open("sqlite3", "gorm.db")
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("Failed Database")
@@ -31,7 +34,7 @@ func InitialMigration() {
 }
 
 func AllUsers(w http.ResponseWriter, r *http.Request) {
-	db, err := gorm.Open("test", "gorm.db")
+	db, err := gorm.Open("sqlite3", "gorm.db")
 	if err != nil {
 		panic("Database could not connect")
 	}
@@ -44,13 +47,56 @@ func AllUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func NewUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Create New User")
+	db, err := gorm.Open("sqlite3", "gorm.db")
+	if err != nil {
+		panic("Database could not connect")
+	}
+
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+	db.Create(&User{Name: name, Email: email})
+	fmt.Fprintf(w, "New User Created Success")
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Delete User")
+	db, err := gorm.Open("sqlite3", "gorm.db")
+	if err != nil {
+		panic("Database could not connect")
+	}
+
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	var user User
+	db.Where("name : ?", name).Find(&user)
+	db.Delete(&user)
+
+	fmt.Fprintf(w, "User successfully Deleted")
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Update User")
+	db, err := gorm.Open("sqlite3", "gorm.db")
+	if err != nil {
+		panic("Database could not connect")
+	}
+
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	var user User
+	db.Where("name : ?", name).Find(&user)
+
+	user.Email = email
+
+	db.Save(&user)
+
+	fmt.Fprintf(w, "User successfully Updated")
 }
