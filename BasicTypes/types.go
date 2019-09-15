2408fdb2270f6624f6cdc31378c43fc3deffbb86
zonieed hossain <zonieed.uap@gmail.com>
2019-09-15 19:29:53 +0600

    All Types basic

--- BasicTypes/types.go
+++ BasicTypes/types.go
@@ -98,5 +98,86 @@ func main(){
 	if err != nil {
 		log.Fatalf("fmt.Sscanf is fail %s\n", err)
 	}
-	fmt.Printf("Sscanf %f\n", s3f)
-	}	
+	fmt.Printf("%f\n", s3f)
+
+	// Character is 2 types byte and rune 
+	// byte is unit8 type and rune is int32 type (byte = 1 byte value and rune = unicode (encode UTF-8 and 1-4 bytes))
+
+	cs := "zonieed"
+	for i:=0; i<len(cs); i++{
+		c := cs[i]
+		fmt.Printf("byte of index %d is '%c' (0x%x)\n", i, c, c)
+	}
+
+	sc := "zonieed"
+	for i,runeChar := range sc {
+		fmt.Printf("Rune of the byte position %d is %#U\n",i, runeChar)
+	}
+
+	 var z string
+	 z1 := "zonieed is a good boy "
+	 z2 := "zonieed is bad boy"
+	 fmt.Printf("%s\n",z+z1+z2)
+
+	 var ar1 = [5]byte{1,2,3,4,5}
+	ar2 := []byte{1,2}
+	fmt.Printf("Length  of array %d %d\n",ar1, ar2)
+	fmt.Printf("Length  of array %d %d\n",len(ar1), len(ar2))
+	m := make(map[string]int)
+	m ["number3"] = 3
+	ck := func(k string){
+		if n, ok := m[k]; ok{
+			fmt.Printf("value of key %s is  %d\n", k, n)
+		}else{
+			fmt.Printf("key doesn't %s exist\n", k)
+		}
+	} 
+	ck("number3")
+	ck("number4")
+	delete(m, "number3")
+	fmt.Printf("key deleted\n")
+	ck("number3")
+	
+	var p int= 4
+	pa := &p
+	fmt.Printf("Here is the pointer address %p and the value %d\n", pa, p)
+
+	// channel is used for goroutine to communicate with each other in a thread safe manner 
+	ch := make(chan int)
+
+	go func() { ch <- 3}()
+
+	n := <-ch
+	fmt.Printf("passed: %d\n", n)
+
+	// zero values
+	var zb bool
+	fmt.Printf("bool: %v\n",zb)
+	var zi int
+	fmt.Printf("int: %v\n",zi)
+	var zs string
+	fmt.Printf("string: %v\n",zs)
+	var zf float64
+	fmt.Printf("floact64: %v\n",zf)
+	var zss []uint32
+	fmt.Printf("slice: %v\n",zss)
+	var zp *int
+	fmt.Printf("pointer: %v\n",zp)
+	var zm map[string]int
+	fmt.Printf("map: %v\n",zm)
+	var zin interface{}
+	fmt.Printf("interface: %v\n",zin)
+	var zc chan bool
+	fmt.Printf("chan: %v\n",zc)
+	var za [5]int
+	fmt.Printf("array: %v\n",za)
+	type struc struct{
+		a int
+		b string
+	}
+	var zst struc
+	fmt.Printf("struct: %v\n",zst)
+	var zff func(bool)
+	fmt.Printf("func: %v\n",zff)
+
+}	
