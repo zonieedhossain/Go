aaaadf8606df3af2d53de9c696a52e35acc80972
zonieedhossain <50265656+zonieedhossain@users.noreply.github.com>
2019-11-10 18:23:39 +0600

    Update elastic.go

--- elastic.go
+++ elastic.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
 
 	elastic "gopkg.in/olivere/elastic.v5"
 )
@@ -15,23 +17,26 @@ type Group struct {
 	Suggest *elastic.SuggestField `json:"suggest_field,omitempty"`
 }
 
-const mapping = `{
-	"settings";{
-		"number_of_shards":2,
-		"number_of_shards": 0
+const mapping = `
+{
+	"settings":{
+		"number_of_shards": 2,
+		"number_of_replicas": 0
 	},
 	"mappings":{
-		"posts":{
-			"user":{
-				"type":"keyword"
-			},
-			"massege":{
-				"type":"text",
-				"store": true,
-				"data":true
-			},
-			"suggest":{
-				"type":"completion"
+		"group":{
+			"properties":{
+				"user":{
+					"type":"keyword"
+				},
+				"massege":{
+					"type":"text",
+					"store": true,
+					"fielddata": true
+				},
+				"suggest_field":{
+					"type":"completion"
+				}
 			}
 		}
 	}
@@ -43,6 +48,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	esver, err := client.ElasticsearchVersion("http://127.0.0.1:9200")
 	if err != nil {
 		panic(err)
@@ -70,10 +76,10 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("Index group %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
-	group2 := `{"user" : "olivere", "message" : "It's a Raggy Waltz"}`
+	group2 := `{"user" : "zonieed", "message" : "It's a Raggy Waltz"}`
 	put2, err := client.Index().
-		Index("twitter").
-		Type("tweet").
+		Index("facebook").
+		Type("group").
 		Id("2").
 		BodyString(group2).
 		Do(ctx)
@@ -120,4 +126,47 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	var grp Group
+	for _, item := range searchResult.Each(reflect.TypeOf(grp)) {
+		if t, ok := item.(Group); ok {
+			fmt.Printf("Group by %s: %s\n", t.User, t.Massege)
+		}
+	}
+	fmt.Printf("Found a total of %d groups\n", searchResult.TotalHits())
+	if searchResult.Hits.TotalHits > 0 {
+		fmt.Printf("Found a total of %d groups\n", searchResult.Hits.TotalHits)
+
+		for _, hit := range searchResult.Hits.Hits {
+			var t Group
+			err := json.Unmarshal(*hit.Source, &t)
+			if err != nil {
+				fmt.Println("Deserialization failed")
+			}
+
+			fmt.Printf("Group by %s: %s\n", t.User, t.Massege)
+		}
+	} else {
+		fmt.Print("Found no Groups\n")
+	}
+	if g := os.Getenv("UPDATE"); g == "1" {
+		for {
+			script := elastic.NewScript("ctx._source.posts += params.num").Param("num", 1)
+			update, err := client.Update().Index("facebook").Type("group").Id("1").
+				Script(script).
+				Upsert(map[string]interface{}{"posts": 0}).
+				Do(context.Background())
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("New version of group %q is now %d\n", update.Id, update.Version)
+		}
+	}
+
+	deleteIndex, err := client.DeleteIndex("facebook").Do(ctx)
+	if err != nil {
+
+		panic(err)
+	}
+	if !deleteIndex.Acknowledged {
+	}
 }
