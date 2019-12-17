8536b4094ed656885ffb9e754d02e5c5e9943169
zonieedhossain <zonieed.uap@gmail.com>
2019-12-17 17:22:21 +0600

    gcs upload file

--- upload/main.go
+++ upload/main.go
@@ -3,7 +3,21 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"mime/multipart"
 	"net/http"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
+)
+
+var (
+	serviceAccountName string
+	serviceAccountID   string
+	uploadableBucket   string
+	cfg                *jwt.Config
 )
 
 func Uploadfile(w http.ResponseWriter, r *http.Request) {
@@ -21,29 +35,93 @@ func Uploadfile(w http.ResponseWriter, r *http.Request) {
 	defer image.Close()
 	fmt.Printf("Upload Image: %+v\n Image Size: %+v\n MIME Header: %+v\n", handler.Filename, handler.Size, handler.Header)
 
+	err = GandU(image, "image")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	// create a temp derectory and file
-	tempFile, err := ioutil.TempFile("temp-image", "upload-*png")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer tempFile.Close()
-
-	imagebytes, err := ioutil.ReadAll(image)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	tempFile.Write(imagebytes)
+	// tempFile, err := ioutil.TempFile("temp-image", "upload-*png")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer tempFile.Close()
+	//
+	// imagebytes, err := ioutil.ReadAll(image)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// tempFile.Write(imagebytes)
 
 	//if success to upload then return
 	fmt.Fprintf(w, "successfully uploaded")
+}
+
+func GandU(file multipart.File, image string) error {
+	url, err := GenSignedurl(fmt.Sprintf("demo/%s", image))
+	if err != nil {
+		return err
+	}
+	err = uploadIntoBucket(file, url)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func uploadIntoBucket(file multipart.File, url string) error {
+	req, err := http.NewRequest(http.MethodPut, url, file)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "image/jpeg")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp)
+	return nil
 }
 func Routes() {
 	http.HandleFunc("/upload", Uploadfile)
 	http.ListenAndServe(":8081", nil)
 }
+
+func GenSignedurl(name string) (string, error) {
+	jsonkey, err := ioutil.ReadFile("/home/xtreme/go/src/test/BasicGo/upload/demo.json")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", jsonkey)
+	cfg, err = google.JWTConfigFromJSON(jsonkey, "https://oauth2.googleapis.com/token")
+	if err != nil {
+		panic(err)
+	}
+	uploadableBucket = "hello-consul"
+	serviceAccountName = cfg.Email
+	serviceAccountID = fmt.Sprintf(
+		"projects/%s/serviceAccounts/%s",
+		"project",
+		serviceAccountName,
+	)
+	url, err := storage.SignedURL(uploadableBucket, name, &storage.SignedURLOptions{
+		GoogleAccessID: serviceAccountName,
+		Method:         "PUT",
+		Expires:        time.Now().Add(15 * time.Minute),
+		ContentType:    "image/jpeg",
+		PrivateKey:     cfg.PrivateKey,
+	})
+	if err != nil {
+
+		return "", err
+	}
+	return url, err
+}
 func main() {
 	fmt.Println("starting upload file")
+
 	Routes()
 }
